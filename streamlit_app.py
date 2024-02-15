0858eab8b8fb51c7965f254c1ae2d67c51d71a16
RomanKhakhalev <RomanKhakhalev@users.noreply.github.com>
2024-02-15 15:05:35 +0000

    alpha version

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if ID_file:
         tabs = st.tabs(['Lois Griffin','Steven Bell', 'Elon Musk'])
         for i in range(3):
             with tabs[i]:
-                st.header(f"Total authenticy score: {SCORES[i][0]})")  
+                st.header(f"Total authenticy score: {SCORES[i][0]}")  
                 cols= st.columns(2)
                 with cols[0]:
                     st.image(f'Data/Results/{i}.png', caption="Uploaded image", width = None, use_column_width = None)
