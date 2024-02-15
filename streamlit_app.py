57700e36d74e6e05382bfc4e5e464d19967fe814
RomanKhakhalev <RomanKhakhalev@users.noreply.github.com>
2024-02-15 13:45:13 +0000

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,10 @@ if ID_file:
         if counter > MAX_COUNTER:
             counter = 0
         with tabs[0]:
-        	 st.image(f'Data/Photos/{counter}.png', caption="Original", width=None, use_column_width=None)
+        	 st.header('Please, enter the initial parameters')
+             #st.image(f'Data/Photos/1.png', caption="Original", width=None, use_column_width=None)
         with tabs[1]:
-        	st.image(f'Data/Results/{counter}.png', caption="Results", width=None, use_column_width=None)
+        	#t.image(f'Data/Results/{counter}.png', caption="Results", width=None, use_column_width=None)
     with col2:
         st.markdown("Document validity:86%")
         st.markdown("Photo validity: 33%")
