e540a9ee1fd4ebc97e8aeee181ece197966b75d6
RomanKhakhalev <RomanKhakhalev@users.noreply.github.com>
2024-02-15 14:35:31 +0000

    alpha version

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,24 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 
+SCORES = [
+         
+         ['10%','30%','50%','60%'],
+         ['10%','30%','50%','60%'],
+         ['10%','30%','50%','60%']
+
+         ]
+
 with st.sidebar:
     st.header('Please, enter the initial parameters')
     ID_file = st.file_uploader('Please upload ID document', type = ['jpg','png','jpeg', 'bmp','tiff'])
 
-with st.container():    
-     st.header("Total authenticy score: :green[30]")  
 
 if ID_file:
-    tabs = st.tabs(['Lois Griffin','Steven Bell', 'Elon Musk'])
     for i in range(3):
+        with st.container():    
+           st.header(f"Total authenticy score: :green[{SCORES[i][0]}]")  
+        tabs = st.tabs(['Lois Griffin','Steven Bell', 'Elon Musk'])
         with tabs[i]:
             cols= st.columns(2)
             with cols[0]:
@@ -25,7 +33,7 @@ if ID_file:
                     st.subheader("Likelihood of image being AI generated: :green[30%]")
                     st.subheader("Likelihood of photo being photoshopped: :green[30%]")    
                     st.subheader("MRZ number incongruence: :green[30%]")     
-                    
+                        
         # with tabs[i]:
         #      st.image(f'Data/Photos/2.png', caption="Original", width=None, use_column_width=None)
         #      st.image(f'Data/Results/2.png', caption="Original", width=None, use_column_width=None)
