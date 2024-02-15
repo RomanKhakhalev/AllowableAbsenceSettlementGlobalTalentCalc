67d7f67b7ac5750360d6b17a87e6653769bf11c2
RomanKhakhalev <RomanKhakhalev@users.noreply.github.com>
2024-02-15 14:05:15 +0000

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,24 +10,19 @@ with st.sidebar:
     st.header('Please, enter the initial parameters')
     ID_file = st.file_uploader('Please upload ID document', type = ['jpg','png','jpeg', 'bmp','tiff'])
 
-counter = 0
-MAX_COUNTER = 2
+
 if ID_file:
     tabs = st.tabs(['Original Photo','Checking results'])
-    counter += 1
-    if counter > MAX_COUNTER:
-        counter = 0
-    
-    with tabs[0]:
-        cols= st.columns(2)
-        with cols[0]:
-            st.image(f'Data/Photos/1.png', caption="Original", width=None, use_column_width=None)
-        with cols[1]:
-            st.image(f'Data/Results/1.png', caption="Original", width=None, use_column_width=None)
-    with tabs[1]:
-         st.image(f'Data/Photos/2.png', caption="Original", width=None, use_column_width=None)
-         st.image(f'Data/Results/2.png', caption="Original", width=None, use_column_width=None)
-    # with col2:
+    for i in range(3):
+        with tabs[i]:
+            cols= st.columns(2)
+            with cols[0]:
+                st.image(f'Data/Photos/{i}.png', caption="Original", width=None, use_column_width=None)
+            with cols[1]:
+                st.image(f'Data/Results/{i}.png', caption="Original", width=None, use_column_width=None)
+        with tabs[1]:
+             st.image(f'Data/Photos/2.png', caption="Original", width=None, use_column_width=None)
+             st.image(f'Data/Results/2.png', caption="Original", width=None, use_column_width=None)
     #     st.markdown("Document validity:86%")
     #     st.markdown("Photo validity: 33%")
 
